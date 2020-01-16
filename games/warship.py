c27d5820faa62d94d079761bee8425d3e03d9ea7
lwz7512 <lwz7512@gmail.com>
2020-01-15 22:55:19 -0400

    ...

--- games/warship.py
+++ games/warship.py
@@ -5,6 +5,31 @@
 # from pygame.constants import HWSURFACE, SRCALPHA
 
 
+
+# ======== Player =============
+class Player(Actor):
+
+  HSPEED = 0
+
+  def __init__(self, startpoint, **kwargs):
+    super(Player, self).__init__('ship_s', **kwargs)
+    self.frame = 0
+    self.pos = startpoint
+
+  def move(self, dt, bounds):
+    if self.x + self.HSPEED > 20 and self.x + self.HSPEED < bounds[0]-20:
+      self.x += self.HSPEED
+
+  def toLeft(self):
+    self.HSPEED = -4
+
+  def toRight(self):
+    self.HSPEED = 4
+  
+  def stopMove(self):
+    self.HSPEED = 0
+
+
 # ======== Alien UFO ==========
 class Alien(Actor):
 
@@ -14,7 +39,6 @@ class Alien(Actor):
   def __init__(self, startpoint, **kwargs):
     super(Alien, self).__init__('alien1', **kwargs)
     self.frame = 0
-    self.stp = startpoint
     self.pos = startpoint
     self.images = ['alien1', 'alien2', 'alien3']
 
@@ -37,15 +61,10 @@ class Alien(Actor):
 
 WIDTH = 500
 HEIGHT = 350
-HSPEED = 0
 
-# init actors first
-ship     = Actor('ship_s')
-ship.pos = 250, 300
-# missile  = Actor('bomb_s')
-missiles = []
+ship     = Player((250, 300))
 ufo      = Alien((20, 30))
-
+missiles = []
 
 # draw() after update() each frame
 def draw():
@@ -53,49 +72,46 @@ def draw():
   # first draw background
   screen.blit('earth-moon', (0, 0))
   # then draw missiles
-  for missile in missiles: # waiting for missle add
-    missile.draw()
+  _renderMissiles()
   # last ship on the top of missiles
   ship.draw()
   # one uft
   ufo.draw()
 
+def _renderMissiles():
+  for missile in missiles: # waiting for missle add
+    missile.draw()
 
 def update(dt):
-  print('ship x:', ship.x)
   # move ship horizontally
-  # if ship.pos[0] >= 30 and ship.pos[0] <= WIDTH-30:
-  if ship.x + HSPEED > 30 and ship.x + HSPEED < WIDTH-30:
-    ship.x += HSPEED
+  ship.move(dt, (WIDTH, HEIGHT))
   # move ship vertically ?
-  
-  # print('update...')
-  for missile in missiles: # update position
-    missile.y -= 3
-    if(missile.y < 0):
-      missiles.remove(missile)
-  
+  _updateMissiles()
   # print('missiles length:', len(missiles))
   ufo.update()
   ufo.move(dt, (WIDTH, HEIGHT))
 
+def _updateMissiles():
+  for missile in missiles: # update position
+    missile.y -= 3
+    if(missile.y < 0):
+      missiles.remove(missile)
+
+def _updateMissiles():
+  pass
 
 def on_key_down(key):
   global HSPEED, FIRE
 
   if key == keys.SPACE:
     print('create new missile')
-    missile = Actor('bomb_s')
-    missile.pos = ship.pos[0], ship.pos[1] - 20
-    missiles.append(missile)
-    sounds.fire.play()
 
   if key == keys.LEFT :
-    HSPEED = -4
+    ship.toLeft()
     print('move left')
 
   if key == keys.RIGHT:
-    HSPEED = 4
+    ship.toRight()
     print('move right')
 
   if key == keys.UP:
@@ -104,14 +120,8 @@ def on_key_down(key):
   if key == keys.DOWN:
     print('move down')
 
-
 def on_key_up(key):
-  global HSPEED
-  HSPEED = 0
-  if ship.pos[0] < 30:
-    ship.x = 30
-  if ship.pos[0] > WIDTH-30:
-    ship.x = WIDTH-30
+  ship.stopMove()
 
 def on_mouse_move(pos):
   # ship.angle = ship.angle_to(pos)
