cef866dd85a6862e1f8d60032cae53fc4789db5c
lwz7512 <lwz7512@gmail.com>
2020-03-01 19:54:04 -0400

    add drawing

---
+++ arcade/open_window_with_function.py
@@ -0,0 +1,15 @@
+import arcade
+
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 640
+
+
+
+def main():
+    
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Arcade")
+
+    arcade.run()
+
+
+main()
