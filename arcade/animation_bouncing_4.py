91d253feeb1d8617ed76d020268e827fe259dc48
lwz7512 <lwz7512@gmail.com>
2020-03-15 12:11:17 -0300

    update

--- arcade/animation_bouncing_4.py
+++ arcade/animation_bouncing_4.py
@@ -14,8 +14,8 @@ class MyGame(arcade.Window):
         # Attributes to store where our ball is
         self.ball_x = 300
         self.ball_y = 240
-        self.ball_speed_x = 3
-        self.ball_speed_y = 3
+        self.ball_speed_x = 6
+        self.ball_speed_y = 6
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
@@ -26,10 +26,12 @@ class MyGame(arcade.Window):
         """ Called to update our objects. Happens approximately 60 times per second."""
         self.ball_x += self.ball_speed_x
         self.ball_y += self.ball_speed_y
-        # if self.ball_x > 640 or self.ball_x < 0:
-        #   self.ball_speed_x = -self.ball_speed_x
-        # if self.ball_y > 480 or self.ball_y < 0:
-        #   self.ball_speed_y = -self.ball_speed_y
+        # print(self.ball_speed_x)
+        if self.ball_x > 640 - 15 or self.ball_x < 15:
+          self.ball_speed_x = -self.ball_speed_x
+
+        if self.ball_y > 480 - 15 or self.ball_y < 15:
+          self.ball_speed_y = -self.ball_speed_y
 
 def main():
     window = MyGame(640, 480, "Simple Animation")
