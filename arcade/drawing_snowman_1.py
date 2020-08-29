b850acb0bd10b5a1cf1f2bae8d93ce3b950b1b5f
lwz7512 <lwz7512@gmail.com>
2020-08-29 12:09:19 -0300

    add moon

--- arcade/drawing_snowman_1.py
+++ arcade/drawing_snowman_1.py
@@ -5,7 +5,7 @@ SCREEN_HEIGHT = 600
 
 
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-arcade.set_background_color(arcade.color.DARK_BLUE)
+arcade.set_background_color(arcade.color.BLACK)
 arcade.start_render()
 
 # Draw the ground
@@ -14,7 +14,7 @@ arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.
 # Draw a snow person
 
 # Snow
-arcade.draw_circle_filled(400, 200, 60, arcade.color.WHITE)
+arcade.draw_circle_filled(400, 200, 80, arcade.color.WHITE)
 arcade.draw_circle_filled(400, 280, 50, arcade.color.WHITE)
 arcade.draw_circle_filled(400, 340, 40, arcade.color.WHITE)
 
@@ -22,6 +22,10 @@ arcade.draw_circle_filled(400, 340, 40, arcade.color.WHITE)
 arcade.draw_circle_filled(385, 350, 5, arcade.color.BLACK)
 arcade.draw_circle_filled(415, 350, 5, arcade.color.BLACK)
 arcade.draw_circle_filled(400, 330, 5, arcade.color.BLACK)
+
+# Moon
+arcade.draw_circle_filled(200, 500, 50, arcade.color.WHITE)
+arcade.draw_circle_filled(180, 510, 40, arcade.color.BLACK)
 
 #  Finish and run
 arcade.finish_render()
