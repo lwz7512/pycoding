1ec8983d2ffba666fd66bd5276ad56f908d39bc2
lwz7512 <lwz7512@gmail.com>
2020-10-17 10:27:37 -0300

    add text to play sound example

--- arcade/play_sound_9.py
+++ arcade/play_sound_9.py
@@ -8,6 +8,14 @@ class MyApplication(arcade.Window):
         # Load the sound when the application starts
         self.laser_sound = arcade.load_sound(":resources:sounds/coin1.wav")
 
+    def on_draw(self):
+        arcade.start_render()
+        
+        start_x = 50
+        start_y = 150
+        arcade.draw_text("Press space key to play sound", start_x, start_y, arcade.color.WHITE, 12)
+
+    
     def on_key_press(self, key, modifiers):
 
         # If the user hits  the space bar, play the sound that we loaded
